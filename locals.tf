8fd72a3e97ec968fff43bbc7e5bd49729760ade2
ash2code <ashoksoma95@gmail.com>
2024-05-19 19:19:52 +0530

    modified code

--- locals.tf
+++ locals.tf
@@ -1,3 +1,7 @@
 locals {
-  env = "dev"
+  subnet_cidr_blocks = distinct(var.public_subnet_cidr_list)
+}
+
+locals {
+  resource_name = "cloud"
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
 variable "availability_zone" {}
 variable "allowed_ssh_cidr_blocks" {}
 variable "egress_cidr_blocks" {}
 variable "ami" {} 
 variable "instance_type" {}
 variable "bucket_name" {}
+variable "public_subnet_cidr_list" {}
+variable "environment" {}
+
 
