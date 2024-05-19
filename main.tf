4acb69c74d125e20f5ee97f1f97517258c30125c
ash2code <ashoksoma95@gmail.com>
2024-05-19 19:59:51 +0530

    issues fixed

--- main.tf
+++ main.tf
@@ -99,12 +99,13 @@ resource "aws_network_acl" "aws-network-acl" {
 }
 
 resource "aws_instance" "aws-ec2" {
+    count = var.environment == "dev" ? 3:1
     ami = var.ami
     instance_type = var.instance_type
-    subnet_id = aws_subnet.aws-subnet.id
+    subnet_id = aws_subnet.aws-subnet[count.index].id
     vpc_security_group_ids = [aws_security_group.aws-security-group.id]
     tags = {
-        Name = "${local.resource_name}-ec2"
+        Name = "${local.resource_name}-ec2-${count.index + 1}"
         env = upper("dev")
     }
     user_data = <<-EOF
